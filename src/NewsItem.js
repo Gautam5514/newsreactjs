7be6d87e5a068c62d2d849421de03e94e16ea00e
Gautam pandit <gautampandit797@gmail.com>
2023-06-16 12:51:27 +0530

    added the new \

--- src/NewsItem.js
+++ src/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
         return (
             <div className="my-3">
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageUrl} className="card-img-top" alt="..."/>
+                    <img src={!imageUrl?"https://img.etimg.com/thumb/msid-101000807,width-210,height-158,imgsize-66440,,resizemode-75/fed-holds-rates-steady-sees-two-small-hikes-by-end-of-year.jpg":imageUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
--- src/component/News.js
+++ src/component/News.js
@@ -2,69 +2,68 @@ import React, { Component } from 'react'
 import NewsItem from '../NewsItem'
 
 export class News extends Component {
-    articles =  [
-        {
-            "source": {
-                "id": "news-com-au",
-                "name": "News.com.au"
-            },
-            "author": "Andrew McMurtry",
-            "title": "Cricket world blown away by Bluey episode",
-            "description": "<p>Cricket&rsquo;s World Test Championship Final rated its socks off across the five days of action but Australia&rsquo;s latest triumph has been put in its place by arguably even bigger cricket event.</p>",
-            "url": "https://www.news.com.au/sport/cricket/bluey-cricket-episode-leaves-cricket-world-in-tears/news-story/018621fc32799baa52f673642155faf7",
-            "urlToImage": "https://content.api.news/v3/images/bin/a2a371f14b3ec9c965ac0ba685307a1c",
-            "publishedAt": "2023-06-14T05:29:00Z",
-            "content": "Cricket’s World Test Championship Final rated its socks off across the five days of action but Australia’s latest triumph has been put in its place by arguably even bigger cricket event.\r\nThe behemot… [+4666 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-    ]
+
     constructor(){
         super();
-        console.log("Hello I am a constructor from news components");
         this.state = {
-            articles: this.articles,
-            loading: false
+            articles: [],
+            loading: false,
+            page:1
         }
     }
-    render() {
+
+    async componentDidMount(){ 
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=6c9aed976465499d98fc00b4f6b0ea5f&page=1pageSize=20";
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData); 
+        this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+    }
+
+    handlePrevClick = async ()=>{
+        console.log("Previous");
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6c9aed976465499d98fc00b4f6b0ea5f&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);  
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+
+    }
+    
+    handleNextClick = async ()=>{
+        console.log("Next"); 
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+        }
+        else{
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6c9aed976465499d98fc00b4f6b0ea5f&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json()
+            console.log(parsedData);  
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+    }
+    }
+
+    render() { 
         return (
             <div className="container my-3">
-                <h2>NewsMonkey - Top Headlines</h2>
-                <div className="row">
-                    <div className="col-md-4">
-                            <NewsItem title="myTitle" description="mydesc" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg" newsUrl="TODO"/>
-                    </div>
-                    <div className="col-md-4">
-                            <NewsItem title="myTitle" description="mydesc"/>
-                    </div>
-                    <div className="col-md-4">
-                            <NewsItem title="myTitle" description="mydesc"/>
-                    </div>
+                <h1>NewsMonkey - Top Headlines</h1> 
+                <div className="row"> 
+                {this.state.articles.map((element)=>{
+                    return <div className="col-md-4" key={element.url}>
+                        <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                    </div> 
+                })} 
+                </div> 
+                <div className="container d-flex justify-content-between">
+                <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+                <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
             </div>
         )
